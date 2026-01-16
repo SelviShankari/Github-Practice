a8139249194dbb1f4451037fd1f391c0047327a6
Selvi Shankari <selvimuthu2504@gmail.com>
2026-01-16 22:20:24 +0530

    Yes deleted password

--- index.py
+++ index.py
@@ -1,3 +1,2 @@
-print("This is the 4th day im learning git and github")
-print("This is the 5th day im leanrning git and github")
-print("Trying to pull from github to local")
+a = "test@12345"
+print(a)
